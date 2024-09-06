8141a840767a4f35f3bb0d55d06afaa5d2024ed8
Darker-Ink <rstonehouse66@gmail.com>
2024-09-06 18:48:44 -0500

    feat: icons

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"github.com/cshum/imagor/loader/httploader"
 	"github.com/cshum/imagor/vips"
 	"github.com/gin-gonic/gin"
+	"regexp"
 )
 
 func main() {
@@ -32,13 +33,35 @@ func main() {
 	r.GET("/frame/*url", handlers.GetFirstFrameHandler)
 	r.GET("/stream/*url", handlers.StreamVideoHandler)
 	r.GET("/thumbhash/*url", handlers.GetThumbHash)
+
 	r.GET("/avatar/:id/:hash", func(c *gin.Context) {
 		handlers.HandleAvatar(app, ctx, c)
 	})
+
 	r.GET("/file/:channelId/:fileId/:fileName", func(c *gin.Context) {
 		handlers.HandleFile(app, ctx, c)
 	})
 
+	r.GET("/logo/:logo", func(c *gin.Context) {
+		// ? Logo's are stored ./logos. Each wone is like icon-0.png, icon-1.png, etc.
+		logo := c.Param("logo")
+
+		if logo == "" {
+			c.JSON(400, gin.H{"error": "Invalid logo"})
+			
+			return
+		}
+
+		match := regexp.MustCompile(`^icon-\d+.png$`).MatchString(logo)
+
+		if !match {
+			c.JSON(400, gin.H{"error": "Invalid logo"})
+			
+			return
+		}
+
+		c.File("./logos/" + logo)
+	})
 
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
